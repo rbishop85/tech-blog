be2bc06677d5a4b9a3bb1bb3f48c7bc0e5889734
Robert Bishop <rbishop85@gmail.com>
2022-08-30 22:42:30 -0700

    setup posts and comments seed files

--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,8 @@ const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
-// const postData = require('./postData.json');
-// const commentData = require('./commentData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -13,9 +13,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-//   await Post.bulkCreate(postData);
+  await Post.bulkCreate(postData);
 
-//   await Comment.bulkCreate(commentData);
+  await Comment.bulkCreate(commentData);
 
   process.exit(0);
 };
