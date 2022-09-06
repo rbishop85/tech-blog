1c1aae8f9aea1cf5fa913658a29e00d2eff1c033
Robert Bishop <rbishop85@gmail.com>
2022-09-05 22:26:32 -0700

    got initial server running using activity 24 example with data converted to match my user models

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// CREATE new user
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
   try {
     // Find the user who matches the posted username
