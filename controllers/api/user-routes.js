4c202be380f3bb955d45baa408766962cf7f2c87
Robert Bishop <rbishop85@gmail.com>
2022-09-05 18:20:03 -0700

    copied in some boiler plate user routes and worked on updating them to match current project

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/login', async (req, res) => {
+  try {
+    // Find the user who matches the posted username
+    const userData = await User.findOne({ where: { username: req.body.username } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    // Verify the posted password with the password stored in the database
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    // Create session variables based on the logged in user
+    req.session.user_id = userData.id;
+    req.session.logged_in = true;
+    req.session.save(() => {
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    // Remove the session variables
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
