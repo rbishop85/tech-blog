e08b816ec0994536a66ef35f759510b0a98e2538
Robert Bishop <rbishop85@gmail.com>
2022-09-05 21:48:05 -0700

    did some work on setting up html routes, user routes and route indexes

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,8 +1,47 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
 // Client side routes/views
 
 // Route "/"
+// Prevent non logged in users from viewing the homepage
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render('homepage', {
+      users,
+      // Pass the logged in flag to the template
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+
+
 
 // Route "/login"
+
+router.get('/login', (req, res) => {
+    // If a session exists, redirect the request to the homepage
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 // Route "/dashboard"
 
@@ -11,3 +50,6 @@
 // Route "/dashboard/edit/:id" <-- edit post
 
 // Route "/post/:id"
+
+
+module.exports = router;
