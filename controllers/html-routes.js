f63ff98ec4b977223191e99ce235f3323bbebd49
Robert Bishop <rbishop85@gmail.com>
2022-09-15 22:10:33 -0700

    starting re-design of login page

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 // Client side routes/views
 
@@ -42,12 +42,69 @@ router.get('/login', (req, res) => {
   });
 
 // Route "/dashboard"
+router.get('/dashboard', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+      ],
+      order: [['updated_at', 'ASC']],
+    });
+
+    const posts = postData.map((project) => project.get({ plain: true }));
+
+    res.render('dashboard', {
+      // posts,
+      // // Pass the logged in flag to the template
+      // logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 // Route "/dashboard/new" <-- new posts
 
 // Route "/dashboard/edit/:id" <-- edit post
 
 // Route "/post/:id"
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          attributes: ['content', 'user_id', 'created_at'],
+          include: [
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    console.log(post);
+
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 module.exports = router;
