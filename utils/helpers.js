b8295ba1e993089e2ac4278978637f6e09cc884e
Robert Bishop <rbishop85@gmail.com>
2022-09-21 13:10:45 -0700

    updated formatting of individual blog post

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,6 +4,13 @@ module.exports = {
         return date.toLocaleString();
       },
 
+      format_date_short: (date) => {
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        // Format date as MM/DD/YYYY
+        return date.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'});
+      },
+
+
     format_content: (content) => {
         if(content.length > 500) {
             return (content.substring(0,500) + "...");
